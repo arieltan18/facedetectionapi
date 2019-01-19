906f08c77a8628be7bc10d88c281e3d8ea9ea7f5
Ariel <actan1804@gmail.com>
2019-01-19 21:46:31 +0800

    fixing url

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/profile/:id', (req,res)=> { profile.handleProfileGet(req, res, db) })
 
 app.put('/image', (req,res)=> { image.handleImage(req, res, db) })
 
-app.put('imageurl', (req, res) => { image.handleApiCall(req,res) })
+app.put('/imageurl', (req, res) => { image.handleApiCall(req,res) })
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
 //     // Store hash in your password DB.
