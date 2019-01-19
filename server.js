89c3070ec58e061b3d69f9e851099c38549ea90a
Ariel <actan1804@gmail.com>
2019-01-19 14:06:00 +0800

    changing response

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParse.json());
 app.use(cors())
 
 app.get('/',(req,res)=> {
-	res.send(database.users);
+	res.send('it is working!');
 })
 
 app.post('/signin', (req,res)=> { signin.handleSignIn(req, res, db, bcrypt) })
