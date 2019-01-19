cb4d01c70bcd3eed475f84f71ededf530a2737e2
Ariel <actan1804@gmail.com>
2019-01-19 21:13:03 +0800

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image =  require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-octagonal-75408',
     user : 'postgres',
     password : 'catherine',
     database : 'facedetection'
